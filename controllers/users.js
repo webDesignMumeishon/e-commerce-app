1d87b1c45175a2b4f795d061bf482007f2b465dc
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-12 14:45:31 -0300

    setting up a token authentication

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const {User} = require('../model/user')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 module.exports = {
@@ -15,6 +16,7 @@ module.exports = {
             country: req.body.country,
         })
         user = await user.save()
+        if(!user) return res.json({message:"The user couldn't be created"})
         return res.json(user)
     },
 
@@ -23,14 +25,45 @@ module.exports = {
         .then(r => {
             res.json(r)
         })
+        .catch(err => {
+            console.log(err);
+        })
     },
 
     getSingleUser: (req, res) => {
         //(-) this is to exclude the field 
         User.findById(req.params.id).select('-passwordHash')
         .then(user => {
-            res.json(user)
+            if(!user) return res.json({message: "The user wasn't found"})
+
+            return res.json(user)
+        })
+        .catch(err => {
+            return res.json(err)
         })
     },
+
+    logUser: async (req, res) => {
+        // first we check if the user exists
+        const user = await User.findOne({email: req.body.email})         
+        if(!user) return res.status(400).send("The user was not found")
+
+        // after we make sure the user is in there we get the hash password
+        // important method of bcrypt to comapre the passwords
+        if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+            const token = jwt.sign(
+                {
+                    userId: user.id
+                },
+                process.env.TOKEN_PASSWORD,
+                {expiresIn: '1d'}
+            )
+            return res.status(200).send({user: user.email, token: token})
+        }
+        else{
+            return res.status(400).send("Password in wrong")
+        }
+        // return res.status(200).send(user)
+    }
     
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const {postUser, getUsers, getSingleUser} = require('../controllers/users')
+const {postUser, getUsers, getSingleUser, logUser} = require('../controllers/users')
 
 //routes
 router.get('/', getUsers)
 router.post('/', postUser)
+router.get('/login', logUser)
 router.get('/:id', getSingleUser)
 
 module.exports = router
