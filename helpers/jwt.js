d0d0f5dbafe0b39d05116cb81b47ebb7b0c04130
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-12 18:30:18 -0300

    unless method

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,9 +2,19 @@ const expressJwt = require('express-jwt')
 
 function authJwt(){
     const secret = process.env.TOKEN_PASSWORD
+    // the functions checks in all the routes the token for validation
     return expressJwt({
         secret, 
         algorithms: ['HS256']
+    })
+    // with the unless method we exclude some routes to validate through tokens
+    .unless({
+        path: [
+            '/api/v1/users/login',
+            {url: '/api/v1/users', methods:["GET", "POST"]},
+            {url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"]},
+            {url: /\/api\/v1\/category(.*)/, methods: ["GET", "OPTIONS"]}
+        ]
     })
 }
 
